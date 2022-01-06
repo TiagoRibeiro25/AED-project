74e45982525b45b02e5565743b60bc2ae9eaeb29
TiagoRibeiro25 <92998482+TiagoRibeiro25@users.noreply.github.com>
2022-01-06 17:03:37 +0000

    Icon e Pagina do Sign up

--- Gestor de Cursos.py
+++ Gestor de Cursos.py
@@ -1,4 +1,6 @@
+from os import set_inheritable
 from tkinter import *
+from datetime import datetime
 from PIL import ImageTk,Image
 
 def start_window():
@@ -8,6 +10,7 @@ def start_window():
     start_window.geometry('850x400+500+300')
     start_window.resizable(0,0)
     start_window.configure(background='black')
+    start_window.iconbitmap("code.ico")
         
     def start():
         start_window.title('Online Courses Manager')
@@ -33,11 +36,13 @@ def start_window():
             lbl_Log_In = Label(painel_log_in, text = 'Log In', fg='black', font = ('Helvetica', 20), background='white')
             lbl_Log_In.place(x = 90, y = 0)
 
+            #Username
             lbl_name = Label(painel_log_in, text = 'Name:', fg='black', font = ('Helvetica', 15), background='white')
             lbl_name.place(x = 10, y = 60)
             Entry_name = Entry(painel_log_in, width = 27, background = 'white')
             Entry_name.place(x = 75, y = 66)
 
+            #Password
             lbl_pw = Label(painel_log_in, text = 'Password:', fg='black', font = ('Helvetica', 14), background='white')
             lbl_pw.place(x = 10, y = 95)
             Entry_pw = Entry(painel_log_in, width = 22, background = 'white', show='*')
@@ -48,11 +53,11 @@ def start_window():
             btn_next.place (x = 44,y = 160)
 
             #botao caso não tenha conta
-            btn_no_account = Button(painel_log_in, text = "Don't have an account? Click here", font = ('Helvetica', 10), fg = 'blue', relief='flat', background = 'white', command=Sign_In)
+            btn_no_account = Button(painel_log_in, text = "Don't have an account? Click here", font = ('Helvetica', 10), fg = 'blue', relief='flat', background = 'white', command=Sign_Up)
             btn_no_account.place (x = 25,y = 270)
 
 
-        def Sign_In():
+        def Sign_Up():
             start_window.title('Online Courses Manager - Sign In')
             painel_inicial = PanedWindow(start_window, width = 770, height= 330, background='navajo white')
             painel_inicial.place(x = 40, y = 33)
@@ -66,15 +71,98 @@ def start_window():
             btn_voltar = Button(painel_inicial, text = 'back', font = ('Helvetica', 10), fg = 'black', relief='raised', background = 'navajo white', width=8, height=1, command=start)
             btn_voltar.place (x = 0,y = 0)
 
-            #Painel da direita (log-In)
-            painel_log_in = PanedWindow(painel_inicial, width = 260, height= 310, background='white')
-            painel_log_in.place(x = 500, y = 10)
+            #Painel da direita (Sign-up)
+            painel_sign_up = PanedWindow(painel_inicial, width = 260, height= 310, background='white')
+            painel_sign_up.place(x = 500, y = 10)
 
-            lbl_Log_In = Label(painel_log_in, text = 'Sign In', fg='black', font = ('Helvetica', 20), background='white')
+            lbl_Log_In = Label(painel_sign_up, text = 'Sign Up', fg='black', font = ('Helvetica', 20), background='white')
             lbl_Log_In.place(x = 85, y = 0)
 
+            #Username
+            lbl_name = Label(painel_sign_up, text = 'Name:', fg='black', font = ('Helvetica', 15), background='white')
+            lbl_name.place(x = 10, y = 60)
+            Entry_name = Entry(painel_sign_up, width = 27, background = 'white')
+            Entry_name.place(x = 75, y = 66)
+
+            #Email
+            lbl_email = Label(painel_sign_up, text = 'Email:', fg='black', font = ('Helvetica', 14), background='white')
+            lbl_email.place(x = 10, y = 95)
+            Entry_email = Entry(painel_sign_up, width = 27, background = 'white')
+            Entry_email.place(x = 75, y = 101)
+
+            #Password
+            lbl_pw = Label(painel_sign_up, text = 'Password:', fg='black', font = ('Helvetica', 14), background='white')
+            lbl_pw.place(x = 10, y = 130)
+            Entry_pw = Entry(painel_sign_up, width = 22, background = 'white', show='*')
+            Entry_pw.place(x = 105, y = 136)
+            
+            #Mostrar password
+            def show_pw():
+                pw_info = val.get()
+                if pw_info == 1:
+                    Entry_pw.configure(show='')
+                if pw_info == 0:
+                    Entry_pw.configure(show='*')
+
+            val = IntVar(0)    
+            btn_pw_visible = Checkbutton(painel_sign_up, text = "Show Password", font = ('Helvetica', 7), background = 'white', variable= val, command=show_pw)
+            btn_pw_visible.place (x = 10,y = 155)
+
+            #Tipo de usuário (user/admin)
+            lbl_user_type = Label(painel_sign_up, text = 'Choose which type of user:', fg='black', font = ('Helvetica', 12), background='white')
+            lbl_user_type.place(x = 10, y = 185)
+
+            user_type_selected = StringVar()
+            User = Radiobutton(painel_sign_up, text='User', font = ('Helvetica', 10), background = 'white', variable= user_type_selected, value='user')
+            Admin = Radiobutton(painel_sign_up, text='Admin', font = ('Helvetica', 10), background = 'white', variable= user_type_selected, value='admin')
+            User.place(x = 10, y = 210)
+            Admin.place(x = 150, y = 210)
+            user_type_selected.set('user')   #Por predefinição, o tipo de usuário escolhido será do tipo "user"
+            #user_type_selected.get() - para pegar o valor user/admin
+
+            lbl_info = Label(painel_inicial, text="Username - can't exceed 29 letters\nEmail - must be a valid\nPassword - must have at least\n a letter/number\nUser type - Only one can be selected", fg='black', font = ('Helvetica', 9), background='navajo white')
+            lbl_info.place(x = 260, y = 50)
+
+
+            #Cruzes ao lado direito de cada linha (user,email,password). Caso as informações tenham erros, a funçao verify irá torná-la visivel
+            lbl_red1 = Label(painel_sign_up, text='X', fg='white', font = ('Helvetica', 10), background='white',height=1)
+            lbl_red2 = Label(painel_sign_up, text='X', fg='white', font = ('Helvetica', 10), background='white',height=1)
+            lbl_red3 = Label(painel_sign_up, text='X', fg='white', font = ('Helvetica', 10), background='white',height=1)
+            lbl_red1.place(x = 242, y = 65)
+            lbl_red2.place(x = 242, y = 100)
+            lbl_red3.place(x = 242, y = 135)
+            
+
+            def verify():
+                name_letters = len(Entry_name.get()) #Numero de caracteres do nome
+                email =Entry_email.get()             #Email
+                pw = Entry_pw.get()
+                i = 0
+
+                if name_letters > 29 or name_letters == 0:          #Máximo 29 caracteres para evitar nomes muito grandes
+                    lbl_red1.configure(text='X', fg='red')
+                else:
+                    lbl_red1.configure(text='V', fg='green')
+
+                if email.find('@') == -1 or email.find('.') == -1:  #verifica se o email tem um '@' e um '.'
+                    lbl_red2.configure(text='X', fg='red')
+                elif email.rfind('.') < email.find('@'):            #Verifica se o '.' está depois do '@'  (xxxx@xxx.xx)
+                    lbl_red2.configure(text='X', fg='red')
+                else:
+                    lbl_red2.configure(text='V', fg='green')
+
+                if pw.count(' ') == len(pw) or pw == '':            #Verificar se existe uma senha e se a senha tem alguma letra ou número (nao seja so espaços)
+                    lbl_red3.configure(text='X', fg='red')
+                else:
+                    lbl_red3.configure(text='V', fg='green')
+                                 
+
+            #Botão para verificar e criar conta
+            btn_create_account = Button(painel_sign_up, text = 'Create Account', font = ('Helvetica', 10), fg = 'white', relief='groove', background = 'green', width=15, command=verify)
+            btn_create_account.place (x = 65,y = 240)
+
             #botao caso não tenha conta
-            btn_no_account = Button(painel_log_in, text = "Already have an account? Click here", font = ('Helvetica', 10), fg = 'blue', relief='flat', background = 'white', command=Log_In)
+            btn_no_account = Button(painel_sign_up, text = "Already have an account? Click here", font = ('Helvetica', 10), fg = 'blue', relief='flat', background = 'white', command=Log_In)
             btn_no_account.place (x = 20,y = 270)
 
 
@@ -103,7 +191,6 @@ def start_window():
             lbl_info1.place(x = 40, y = 50)
 
 
-
         painel_inicial = PanedWindow(start_window, width = 770, height= 330, background='navajo white')
         painel_inicial.place(x = 40, y = 33)
 
@@ -124,8 +211,8 @@ def start_window():
         btn_Log_In = Button(painel_inicial, text = 'Log In', font = ('Helvetica', 15), fg = 'black', relief='groove', background = 'SkyBlue2', width=10, height=1, command=Log_In)
         btn_Log_In.place (x = 260,y = 157)
 
-        btn_Sign_In = Button(painel_inicial, text = 'Sign In', font = ('Helvetica', 15), fg = 'black', relief='groove', background = 'SkyBlue2', width=10, height=1, command=Sign_In)
-        btn_Sign_In.place (x = 390,y = 157)
+        btn_Sign_Up = Button(painel_inicial, text = 'Sign Up', font = ('Helvetica', 15), fg = 'black', relief='groove', background = 'SkyBlue2', width=10, height=1, command=Sign_Up)
+        btn_Sign_Up.place (x = 390,y = 157)
 
         btn_Exit = Button(painel_inicial, text = 'Exit', font = ('Helvetica', 15), fg = 'black', relief='groove', background = 'red', width=10, height=1, command = start_window.destroy)
         btn_Exit.place (x = 320,y = 207)
